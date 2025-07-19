ec52d4f73056c94793ff639dbf87c3670fb1c69b
indrajeet59kr@gmail.com <137638568+indrajeet800@users.noreply.github.com>
2025-07-19 16:45:14 +0530

    new

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -2,12 +2,19 @@ package com.test4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+  
 @SpringBootApplication
 public class Test4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
+		String name  = "mike";
+		int x = 100;
+		int y= 200;
+		boolean isPresent = false;
+		float m =10.3f;
+               boolean isSub=true;
+		char cg = "x";
 	}
     // bug fi
 }
