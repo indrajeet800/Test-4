38b6600bf2419199c6e1b4057fdb66d8e41631a7
indrajeet  Kumar <137638568+indrajeet800@users.noreply.github.com>
2025-07-19 16:42:54 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -8,6 +8,12 @@ public class Test4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
+		String name  = "mike";
+		int x = 100;
+		int y= 200;
+		boolean isPresent = false;
+		float m =10.3f;
+		char cg = "x";
 	}
     // bug fi
 }
