7f91542e5b55003dbbf1d07189f2abf4e7547d0d
indrajeet  Kumar <137638568+indrajeet800@users.noreply.github.com>
2025-07-19 16:32:51 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -2,7 +2,7 @@ package com.test4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+  
 @SpringBootApplication
 public class Test4Application {
 
